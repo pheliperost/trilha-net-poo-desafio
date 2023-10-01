9709cb582bc0a0eaf582b8c2c06cfc64b77e420f
Phelipe Rost <phelipe_rost@hotmail.com>
2023-10-01 18:36:40 -0300

    Resultados do desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Nokia nokia = new Nokia("111","modelo 1 nokia", "imei1", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("app 1");
+
+Console.WriteLine("-----------");
+
+
+Console.WriteLine("Iphone: ");
+Iphone iphone = new Iphone("222","modelo 2 iphone", "imei2", 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("app 2");
+
